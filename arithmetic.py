7ff90ac3444b97e8c8deb033042e584b8152cacc
Sarah Ingram <ingramsarah11@gmail.com>
2021-03-17 03:33:00 +0000

    REPL program with Add functionality only

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,49 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-
-    return 10
+    sum = num1 + num2
+    return sum
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
+    total = num1 - num2 
+    return total
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
+    return num1 * num2 
 
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
+    return num1 / num2
 
 
 def square(num1):
     """Return the square of the input."""
+    return num1 * num1
 
 
 def cube(num1):
     """Return the cube of the input."""
+    return num1 * num1 * num1
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    return num1 ** num2
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Return the sum of num1 & num2 multiplied by num3"""
+    return ((num1 + num2) * num3)
+
+def add_cubes(num1, num2):
+    """Return the sum of the cubes of two numbers"""
+    return (num1 * num1 * num1) + (num2 * num2 * num2)
--- calculator.py
+++ calculator.py
@@ -5,3 +5,24 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+# repeat forever 
+while True:
+#     tokenize string
+    user_choice = input("> ")
+    user_choice = user_choice.split(' ')
+    print(user_choice)
+#     if its not equal to q
+    if user_choice[0] == 'q':
+#      end program 
+        print("Exiting Program")
+        break
+    else:
+        num1 = int(user_choice[1])
+        num2 = int(user_choice[2])
+#     check what operation the first item in the list is equal together  
+#         then perform the operation with the 2nd and 3rd item in the list\
+        if user_choice[0] == "+":
+            print(add(num1,num2))
+            
+
